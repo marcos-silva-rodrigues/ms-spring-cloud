194b9cfd1600cf8a3018fb8c1b03fd6efdf1f290
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-05-07 11:44:37 -0300

    chore: init

---
+++ hr-worker/src/test/java/com/rodrigues/silva/marcos/hrworker/HrworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigues.silva.marcos.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
