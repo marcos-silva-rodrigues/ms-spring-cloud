cc00a555f1c8128754b4f66bc5092cf1b84f96f2
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-05-08 15:10:03 -0300

    feat: create eureka server

---
+++ hr-eureka-server/src/main/java/com/rodrigues/silva/marcos/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigues.silva.marcos.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+
+}
