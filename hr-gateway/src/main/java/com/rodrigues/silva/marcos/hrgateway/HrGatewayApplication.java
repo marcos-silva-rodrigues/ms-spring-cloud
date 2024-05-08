cf078b64380cd52ba28639209d4e869b338873bd
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-05-08 16:53:01 -0300

    feat: api gateway config

--- hr-gateway/src/main/java/com/rodrigues/silva/marcos/hrgateway/HrGatewayApplication.java
+++ hr-gateway/src/main/java/com/rodrigues/silva/marcos/hrgateway/HrGatewayApplication.java
@@ -2,12 +2,25 @@ package com.rodrigues.silva.marcos.hrgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
 
 @SpringBootApplication
 public class HrGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+						.route("worker", r -> r.path("/workers/**")
+										.uri("lb://hr-worker"))
+						.route("payment", r -> r.path("/payments/**")
+										.uri("lb://hr-payrool"))
+						.build();
 	}
 
 }
