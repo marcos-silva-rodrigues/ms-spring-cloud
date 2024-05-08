f2d3915c7c7ae6474d83285098aaea7ab4cff34d
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-05-08 16:18:41 -0300

    feat: circuit breaker in payroll api

--- hr-payrool/src/main/java/com/rodrigues/silva/marcos/hrpayrool/resources/PaymentResource.java
+++ hr-payrool/src/main/java/com/rodrigues/silva/marcos/hrpayrool/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.rodrigues.silva.marcos.hrpayrool.resources;
 
 import com.rodrigues.silva.marcos.hrpayrool.entities.Payment;
 import com.rodrigues.silva.marcos.hrpayrool.services.PaymentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +19,15 @@ public class PaymentResource {
   private PaymentService service;
 
   @GetMapping("/{workerId}/days/{days}")
+  @CircuitBreaker(name = "hrworker", fallbackMethod = "fakePayment")
   public ResponseEntity<Payment> payment(@PathVariable Long workerId, @PathVariable Integer days) {
     Payment payment = service.getPayment(workerId, days);
+    return ResponseEntity.ok(payment);
+  }
+
+  public ResponseEntity<Payment> fakePayment(@PathVariable Long workerId, @PathVariable Integer days, Exception ex) {
+
+    Payment payment = new Payment("Brian", 400.0, days);
     return ResponseEntity.ok(payment);
   }
 }
