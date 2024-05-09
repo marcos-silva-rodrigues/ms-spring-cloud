5cf4e7ce1f4f6a8bb79dc83f3738659a2c1ba23d
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-05-09 15:14:42 -0300

    feat: hr worker using config server

--- hr-worker/src/main/java/com/rodrigues/silva/marcos/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/rodrigues/silva/marcos/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.rodrigues.silva.marcos.hrworker.resources;
 
 import com.rodrigues.silva.marcos.hrworker.entities.Worker;
 import com.rodrigues.silva.marcos.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +19,20 @@ import java.util.Optional;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+  private static  final Logger log = LoggerFactory.getLogger(Worker.class);
+
   @Autowired
   private WorkerRepository workerRepository;
+
+  @Value("${test.config}")
+  private String configValue;
+
+
+  @GetMapping("/config")
+  public ResponseEntity config() {
+    log.info(String.format("CONFIG VALUE: %s", configValue));
+    return ResponseEntity.noContent().build();
+  }
 
 
   @GetMapping
